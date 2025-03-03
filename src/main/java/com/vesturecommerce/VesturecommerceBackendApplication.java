44b03d98be0ab33d6223c4650ac0abda93537907
Imanol Villalba <imanolvr96@gmail.com>
2025-03-03 20:50:29 +0100

    Initial commit with project setup

---
+++ src/main/java/com/vesturecommerce/VesturecommerceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vesturecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VesturecommerceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VesturecommerceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vesturecommerce/VesturecommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vesturecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VesturecommerceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
